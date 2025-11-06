6bf120cd5ac5d97f46a3c5a146150bf3e9fc3188
Juan Victor Leite <juanvictorleite@icloud.com>
2025-11-06 12:36:52 -0300

    feat(api): Implement findById for PrismaQuestionsRepository
    This commit implements the `findById` method within the `PrismaQuestionsRepository`.
    It now queries the database using Prisma to retrieve a question by its unique ID.
    The retrieved Prisma model is then mapped to the domain `Question` entity using `PrismaQuestionMapper`.
    
    Additionally, the `updatedAt` property in the `Question` domain entity has been updated
    to explicitly allow `null`, better reflecting its potential state in the database.

---
+++ apps/api/src/database/prisma/mappers/prisma-question-mapper.ts
@@ -0,0 +1,19 @@
+import { Question, Slug, UniqueEntityID } from '@forum/domain';
+import { Question as PrismaQuestion } from 'generated/prisma/client';
+
+export class PrismaQuestionMapper {
+  static toDomain(raw: PrismaQuestion): Question {
+    return Question.create(
+      {
+        title: raw.title,
+        content: raw.content,
+        authorId: new UniqueEntityID(raw.authorId),
+        bestAnswerId: undefined,
+        slug: Slug.create(raw.slug),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id),
+    );
+  }
+}
--- apps/api/src/database/prisma/repositories/prisma-questions-repository.ts
+++ apps/api/src/database/prisma/repositories/prisma-questions-repository.ts
@@ -1,10 +1,24 @@
 import { PaginationParams, Question, QuestionsRepository } from '@forum/domain';
 import { Injectable } from '@nestjs/common';
+import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
-  findById(id: string): Promise<Question | null> {
-    throw new Error('Method not implemented.');
+  constructor(private prisma: PrismaService) {}
+
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!question) {
+      return null;
+    }
+
+    return PrismaQuestionMapper.toDomain(question);
   }
 
   findBySlug(slug: string): Promise<Question | null> {
