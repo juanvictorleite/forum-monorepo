e38b238b40ba4c6434dbbf82e89d83d20eb73227
Juan Victor Leite <juanvictorleite@icloud.com>
2025-12-30 11:21:56 -0300

    test(domain): add missing students repository in test files

--- packages/domain/src/domain/forum/application/use-cases/comment-on-question.spec.ts
+++ packages/domain/src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -2,11 +2,13 @@ import { makeQuestion } from "test/factories/make-question";
 import { InMemoryQuestionAttachmentsRepository } from "test/repositories/in-memory-question-attachments-repository";
 import { InMemoryQuestionCommentsRepository } from "test/repositories/in-memory-question-comments-repository";
 import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { InMemoryStudentsRepository } from "test/repositories/in-memory-students-repository";
 import { CommentOnQuestionUseCase } from "./comment-on-question";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
 let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository;
+let inMemoryStudentsRepository: InMemoryStudentsRepository;
 let sut: CommentOnQuestionUseCase;
 
 describe("Comment on Question", () => {
@@ -16,8 +18,10 @@ describe("Comment on Question", () => {
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
       inMemoryQuestionAttachmentsRepository,
     );
-    inMemoryQuestionCommentsRepository =
-      new InMemoryQuestionCommentsRepository();
+    inMemoryStudentsRepository = new InMemoryStudentsRepository();
+    inMemoryQuestionCommentsRepository = new InMemoryQuestionCommentsRepository(
+      inMemoryStudentsRepository,
+    );
 
     sut = new CommentOnQuestionUseCase(
       inMemoryQuestionsRepository,
